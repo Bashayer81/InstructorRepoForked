721adf78eb94f74c8440b2e63ab97b2a93cbaa36
Bashayer Aljassim <220036584@aston.ac.uk>
2024-01-23 23:13:23 +0000

    Added another comment in gitpod lab1

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
 // Edit made by Bashayer_220036584
+// Edit made by Bashayer_220036584 in gitpod 
 
   //Last line print('Good bye')
 }
